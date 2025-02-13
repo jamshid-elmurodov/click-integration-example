95902b5a8093abf1f43be9e0674a2770d7bfbdb4
Jamshid <elmurodovjamshid38@gmail.com>
2025-02-13 20:05:48 +0500

    bismillah 2

--- src/main/java/uz/clickintegrationexample/domain/click/dto/request/ClickCompleteRequest.java
+++ src/main/java/uz/clickintegrationexample/domain/click/dto/request/ClickCompleteRequest.java
@@ -1,10 +1,6 @@
 package uz.clickintegrationexample.domain.click.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -14,16 +10,12 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 public class ClickCompleteRequest {
-    @JsonProperty("click_trans_id")
     private Long clickTransId;
 
-    @JsonProperty("service_id")
     private Integer serviceId;
 
-    @JsonProperty("merchant_trans_id")
     private String merchantTransId;
 
-    @JsonProperty("merchant_prepare_id")
     private Integer merchantPrepareId;
 
     private Float amount;
@@ -32,12 +24,9 @@ public class ClickCompleteRequest {
 
     private Integer error;
 
-    @JsonProperty("error_note")
     private String errorNote;
 
-    @JsonProperty("sign_time")
     private LocalDateTime signTime;
 
-    @JsonProperty("sign_string")
     private String signString;
 }
--- src/main/java/uz/clickintegrationexample/domain/click/dto/request/ClickPrepareRequest.java
+++ src/main/java/uz/clickintegrationexample/domain/click/dto/request/ClickPrepareRequest.java
@@ -1,10 +1,6 @@
 package uz.clickintegrationexample.domain.click.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -14,13 +10,10 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 public class ClickPrepareRequest {
-    @JsonProperty("click_trans_id")
     private Long clickTransId;
 
-    @JsonProperty("service_id")
     private Integer serviceId;
 
-    @JsonProperty("merchant_trans_id")
     private String merchantTransId;
 
     private Float amount;
@@ -29,12 +22,9 @@ public class ClickPrepareRequest {
 
     private Integer error;
 
-    @JsonProperty("error_note")
     private String errorNote;
 
-    @JsonProperty("sign_time")
     private LocalDateTime signTime;
 
-    @JsonProperty("sign_string")
     private String signString;
 }
